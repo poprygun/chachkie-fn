5f313742b669441cbdd8f49c30844290bb302aac
Alex <poprygun@hotmail.com>
2021-01-08 11:28:00 -0500

    Add SCS.

--- src/main/java/io/microsamples/function/chachkiefn/ChachkieFnApplication.java
+++ src/main/java/io/microsamples/function/chachkiefn/ChachkieFnApplication.java
@@ -1,8 +1,8 @@
 package io.microsamples.function.chachkiefn;
 
 import lombok.Builder;
-import lombok.Data;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.jeasy.random.EasyRandom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,12 +10,19 @@ import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 
 import java.time.Instant;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class ChachkieFnApplication {
 
 	private final EasyRandom easyRandom = new EasyRandom();
+
+	@Bean
+	public Supplier<Flux<String>> names(){
+		return () -> Flux.just("one", "two", "three");
+	}
 
 	@Bean
 	public Function<Flux<String>, Flux<Chachkie>> chachkies() {
@@ -24,6 +31,11 @@ public class ChachkieFnApplication {
 				.name(value)
 				.when(easyRandom.nextObject(Instant.class))
 				.build());
+	}
+
+	@Bean
+	public Consumer<Flux<Chachkie>> chachkiesSink(){
+		return Flux::log;
 	}
 
 	public static void main(String[] args) {
@@ -36,5 +48,6 @@ public class ChachkieFnApplication {
 @Builder
 class Chachkie {
 	String name;
+	String status;
 	Instant when;
 }
